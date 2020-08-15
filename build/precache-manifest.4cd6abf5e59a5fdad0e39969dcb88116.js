2745f4a36dafb0faf1c635c1363bda752413102d
erkartali <erkartali@gmail.com>
2020-08-15 08:07:18 -0500

    added git ignore

---
+++ build/precache-manifest.4cd6abf5e59a5fdad0e39969dcb88116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629fe72ac198a9c1f50dcf50aaf1def5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "28151a254f5327295d6f",
+    "url": "/clicky-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03c5981147b2306e3af8",
+    "url": "/clicky-game/static/js/2.030fdbd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.030fdbd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28151a254f5327295d6f",
+    "url": "/clicky-game/static/js/main.3357ab90.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
